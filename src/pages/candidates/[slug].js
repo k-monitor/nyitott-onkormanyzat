437f53b58bd1b0f057d38aea444c6594e49d523b
boa <boa@protonmail.com>
2024-05-10 16:47:21 +0200

    make url absolute

--- src/pages/candidates/[slug].js
+++ src/pages/candidates/[slug].js
@@ -65,7 +65,7 @@ export default function Page({ pageData, ogImage, records, props, data }) {
         <meta name="description" content={pageData.program} />
         <meta property="og:title" content={pageData.name} />
         <meta property="og:description" content={pageData.program} />
-        <meta property="og:image" content={config.prefix+'/'+ogImage} />
+        <meta property="og:image" content={config.baseUrl+'/'+ogImage} />
         <meta property="og:url" content={config.baseUrl+'candidates/'+pageData.id} />
         <meta property="og:type" content="website" />
       </Head>
