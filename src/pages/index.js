49d7bfcd0eae3fa54ff2865a1aee19f2dd61e849
boa <boa@protonmail.com>
2024-05-17 11:55:47 +0200

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,14 +27,24 @@ export default function Home({ records, data, props }) {
       </Head>
       <main>
         <Logo style={{scale:"2", margin: "80px", fill: '#eee'}} />
-        <h1>Nyitott önkormányzat</h1>
+        <h1>Építsünk Nyitott Önkormányzatot!</h1>
 
-        <p>Ide kéne írni valamit.</p>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-          <a style={{marginRight: '50px'}} href='https://docs.google.com/forms/d/e/1FAIpQLScGZmVAP0Ka_oGt7bhPkUmT7weW139NfeOHW-3ZYNTJT6SaPw/viewform'>jelölt vagyok</a>
-          <a href='/list'>látogató</a>
-          {/* <a className='disabled'>látogató</a> */}
+        <div style={{maxWidth: "800px", marginLeft: "auto", marginRight: "auto"}}>
+          <p>Itt hamarosan a K-Monitor Nyitott Önkormányzatok projekt keretében tett polgármesterjelölti vállalások lesznek böngészhetők egy interaktív térképen!</p>
+
+          <p>Addig is olvasd el <a href="https://drive.google.com/file/d/1bY_8IDRlutW7103vRZYfvnsUeoj8Aosy/view">kiadványunkat</a>, ahol hazai és nemzetközi példákkal illusztráltuk, mit értünk a gyakorlatban a nyitott önkormányzatiság alatt. </p>
         </div>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <a className='button' style={{marginRight: '50px'}} href='https://docs.google.com/forms/d/e/1FAIpQLScGZmVAP0Ka_oGt7bhPkUmT7weW139NfeOHW-3ZYNTJT6SaPw/viewform'>Polgármesterjelölt vagyok, vállalást teszek!</a>
+          <a className='button' href='/list'>Választó vagyok, megnézem a vállalásokat!</a>
+          {/* <a className='disabled button'>látogató</a> */}
+        </div>
+        <footer style={{bottom: "20px", position: "absolute", width: "100%"}}>
+        <p>Értesülj a K-Monitor havi beszámolójából további hasonló projektekről!</p>
+        <p>Iratkozz fel!</p>
+        <p>k-monitor.hu</p>
+        </footer>
+
       </main>
 
       <style jsx>{`
@@ -44,6 +54,10 @@ export default function Home({ records, data, props }) {
           align-items: center;
           justify-content: center;
           padding: 0 20px;
+        }
+
+        footer * {
+          margin: 0;
         }
 
         html, body, #__next, main {
@@ -64,13 +78,22 @@ export default function Home({ records, data, props }) {
           font-size: 1.2em;
         }
 
-        a {
-          background: #eee;
-          padding: 8px;
-          color: #111;
-          border-radius: 5px;
-        }
         a:hover {
+          color: #fff;
+        }
+
+        a {
+          text-decoration: underline;
+        }
+
+        a.button {
+          background: #eee;
+          padding: 12px;
+          color: #111;
+          border-radius: 8px;
+          text-decoration: none;
+        }
+        a.button:hover {
           color: #888;
         }
 
