39e1ec439b3adc9441c6d6bae4079226592504e5
boa <boa@protonmail.com>
2024-05-23 09:19:53 +0200

    add google tag manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,29 @@ import "../header/Menu.transition.css";
 import "../header/Menu.module.css";
 
 function MyApp({ Component, pageProps }) {
+  return <>
+  <script
+  id="tagmanager-main"
+  strategy="afterInteractive"
+  async
+  src={`https://www.googletagmanager.com/gtag/js?id=GTM-MMHD8865`}
+></script>
+<script
+  id="tagmanager-setup"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'GTM-MMHD8865');
+    `,
+  }}
+/>
+  {/* eslint-disable-next-line react/jsx-props-no-​spreading */}
+  <Component {...pageProps} />
+</>
+
   return <Component {...pageProps} />
 }
 
