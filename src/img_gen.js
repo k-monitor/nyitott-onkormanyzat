b82066b327247f41593cac504e70bb73900efcdc
boa <boa@protonmail.com>
2024-05-23 13:35:42 +0200

    fix image sizes for og

--- src/img_gen.js
+++ src/img_gen.js
@@ -18,8 +18,8 @@ export async function generateImage({
   const pngPath = join(process.cwd(), publicPath, relativePath)
   const og =  (
     <div style={{ display: "flex", backgroundColor: 'white', width: '1200px', height: '600px', padding: '40px', }}>
-      <img src={options.img} height="600px" width="450px" style={{ position: "absolute", right: "0", height:"600px", width: "450px" }} ></img>
-      <div style={{width: "1200", height: "1200", position: "absolute", backgroundColor: catToFixColor(options.category), transform: "rotate(70deg) translate(-520px, 250px)", borderRadius: "30px"}}> </div>
+      <img src={options.img} height="600px" width="450px" style={{ position: "absolute", right: "0", height:"600px", width: "400px" }} ></img>
+      <div style={{width: "1200", height: "1200", position: "absolute", backgroundColor: catToFixColor(options.category), transform: "rotate(70deg) translate(-520px, 205px)", borderRadius: "30px"}}> </div>
       <div style={{ display: "flex", flexDirection: 'column', marginLeft: '10px', width: '754px', paddingRight: '10px'}} >
         <h1 style={{color: "white", fontSize: '52px', fontFamily: 'Montserrat-Bold', margin: '0'}}>{'NYITOTT ÖNKORMÁNYZATOT ÉPÍTEK!'}</h1>
         <p style={{color: "white", fontSize: '30px', margin: '0'}}>{options.district}</p>
